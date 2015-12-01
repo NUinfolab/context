8a654858ece8bc7719fc2c57d3b3204640414925
Scott Bradley <scott2b@users.noreply.github.com>
2015-12-01 17:07:29 -0600

    Update setup.py
    Adds requests, nltk and oauthlib to requirements

--- setup.py
+++ setup.py
@@ -8,11 +8,14 @@ setup(
     packages=find_packages(),
     include_package_data=True,
     install_requires=[
+        'requests==2.3.0',
+        'nltk==2.0.5',
         'newspaper',
         'numpy',
         'birdy',
         'flask',
-        'pymongo'
+        'pymongo',
+        'requests-oauthlib'
     ],
     dependency_links = [
     ],
